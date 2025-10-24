243637b24e8d7b6cdd11bbc5748282764f45be7f
Deepak <231001029@rajalakshmi.edu.in>
2025-10-24 23:17:16 +0530

    Time: 1 ms (67.73%), Space: 42.5 MB (94.36%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String i:operations){
+            if(i.indexOf("+")!=-1){
+                x+=1;
+            }
+            else{
+                x-=1;
+            }
+        }
+        return x;
+    }
+}
