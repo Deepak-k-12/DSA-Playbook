d9362139430cc0542a1b0d83059e0b5c6cd80cf7
Deepak <231001029@rajalakshmi.edu.in>
2025-11-10 15:49:59 +0530

    Time: 2 ms (72.29%), Space: 47.8 MB (15.66%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public IList<IList<int>> SubsetsWithDup(int[] nums) {
+        Array.Sort(nums);
+        IList<IList<int>> ans=new List<IList<int>>();
+        Check(0,nums,ans,new List<int>());
+        return ans;
+    }
+
+    public void Check(int i,int[] nums,IList<IList<int>> ans,IList<int> demo){
+        ans.Add(new List<int>(demo));
+        for(int st=i;st<nums.Length;st++){
+            if(st>i && nums[st]==nums[st-1]){continue;}
+            demo.Add(nums[st]);
+            Check(st+1,nums,ans,demo);
+            demo.RemoveAt(demo.Count-1);
+        }
+    }
+
+}
