73c1595e37a7ad78db2737ba4033fc4391b43ec9
Deepak <231001029@rajalakshmi.edu.in>
2025-12-24 01:05:03 +0530

    Time: 1 ms (99.84%), Space: 46.8 MB (11.04%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -5,11 +5,15 @@ class Solution {
         for(int i=0;i<n;i++){
             Arrays.fill(arr[i],'.');
         }
-        solve(0,arr,ans,n);
+
+        int []left=new int[n];
+        int []lower=new int [2*n-1];
+        int []upper=new int [2*n-1];
+        solve(0,arr,ans,n,left,lower,upper);
         return ans;
     }
 
-    public void solve(int col,char[][] arr,List<List<String>> ans,int n){
+    public void solve(int col,char[][] arr,List<List<String>> ans,int n,int[] left,int[] lower,int[] upper){
         if(n==col){
             List<String> st=new ArrayList<>();
             for(int i=0;i<n;i++){
@@ -20,37 +24,43 @@ class Solution {
         }
 
         for(int i=0;i<n;i++){
-            if(safe(i,col,arr,n)){
+            if(left[i]==0 && lower[i+col]==0 && upper[n-1+col-i]==0){
+                left[i]=1;
+                lower[i+col]=1;
+                upper[n-1+col-i]=1;
                 arr[i][col]='Q';
-                solve(col+1,arr,ans,n);
+                solve(col+1,arr,ans,n,left,lower,upper);
                 arr[i][col]='.';
+                left[i]=0;
+                lower[i+col]=0;
+                upper[n-1+col-i]=0;
             }
         }
     }
 
-    public boolean safe(int row,int col,char[][]arr,int n){
-        int a=row;
-        int b=col;
+    // public boolean safe(int row,int col,char[][]arr,int n){
+    //     int a=row;
+    //     int b=col;
 
-        while(col>=0){
-            if(arr[row][col]=='Q'){return false;}
-            col--;
-        }
-        row=a;
-        col=b;
-        while(row>=0 && col>=0){
-            if(arr[row][col]=='Q'){return false;}
-            row--;
-            col--;
-        }
+    //     while(col>=0){
+    //         if(arr[row][col]=='Q'){return false;}
+    //         col--;
+    //     }
+    //     row=a;
+    //     col=b;
+    //     while(row>=0 && col>=0){
+    //         if(arr[row][col]=='Q'){return false;}
+    //         row--;
+    //         col--;
+    //     }
         
-        row=a;
-        col=b;
-        while(row<n && col>=0){
-            if(arr[row][col]=='Q'){return false;}
-            row+=1;
-            col--;
-        }
-        return true;
-    }
+    //     row=a;
+    //     col=b;
+    //     while(row<n && col>=0){
+    //         if(arr[row][col]=='Q'){return false;}
+    //         row+=1;
+    //         col--;
+    //     }
+    //     return true;
+    // }
 }
