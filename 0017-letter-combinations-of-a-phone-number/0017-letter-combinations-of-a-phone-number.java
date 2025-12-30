e885ee024107815b4d5a41419f4e5ab75f20be37
Deepak <231001029@rajalakshmi.edu.in>
2025-12-30 20:03:14 +0530

    Time: 0 ms (100%), Space: 43.2 MB (92.59%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,35 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+
         if (digits == null || digits.length() == 0) {
-             return new ArrayList<>();
-            }
-      HashMap<Character, List<Character>> demo = new HashMap<>();
+            return new ArrayList<>();
+        }
 
-        demo.put('2', Arrays.asList('a', 'b', 'c'));
-        demo.put('3', Arrays.asList('d', 'e', 'f'));
-        demo.put('4', Arrays.asList('g', 'h', 'i'));
-        demo.put('5', Arrays.asList('j', 'k', 'l'));
-        demo.put('6', Arrays.asList('m', 'n', 'o'));
-        demo.put('7', Arrays.asList('p', 'q', 'r', 's'));
-        demo.put('8', Arrays.asList('t', 'u', 'v'));
-        demo.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+        String[] map = {
+                "", "", "abc", "def", "ghi",
+                "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
 
-        ArrayList<String> ans=new ArrayList<>();
-        StringBuilder sb=new StringBuilder();
-        backtrack(0,digits,ans,demo,sb);
+        List<String> ans = new ArrayList<>();
+        backtrack(0, digits, new StringBuilder(), ans, map);
         return ans;
-
     }
 
-    public void backtrack(int ind,String ip,ArrayList<String> ans,HashMap<Character, List<Character>> demo,StringBuilder sb){
-        if(ind==ip.length()){
+    private void backtrack(int idx, String digits,
+            StringBuilder sb,
+            List<String> ans,
+            String[] map) {
+
+        if (idx == digits.length()) {
             ans.add(sb.toString());
             return;
         }
 
-        List<Character> a=demo.get(ip.charAt(ind));
-        for(Character i:a){
-            sb.append(i);
-            backtrack(ind+1,ip,ans,demo,sb);
-            sb.deleteCharAt(sb.length()-1);
+        String letters = map[digits.charAt(idx) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            sb.append(letters.charAt(i));
+            backtrack(idx + 1, digits, sb, ans, map);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
