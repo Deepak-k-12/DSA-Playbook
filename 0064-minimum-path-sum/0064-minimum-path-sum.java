8d23651714383ffa00827eb74eaabc0b7112a20d
Deepak <231001029@rajalakshmi.edu.in>
2026-01-07 19:26:13 +0530

    Time: 6 ms (10.61%), Space: 50.7 MB (11.09%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,11 +3,22 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int [m][n];
-
         for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0]=grid[0][0];
+                    continue;}
+                int down=100000;
+                int right=100000;
+                if(i>0){ 
+                down=grid[i][j]+dp[i-1][j];}
+                if(j>0){
+                    right=grid[i][j]+dp[i][j-1];
+                }
+                dp[i][j]=Math.min(down,right);
+            }
         }
-        return backtrack(m-1,n-1,grid,dp);
+        return dp[m-1][n-1];
         
     }
 
@@ -15,7 +26,7 @@ class Solution {
         if(i==0 && j==0){return grid[0][0];}
         if(i<0 || j<0){return 1000000;}
         if(dp[i][j]!=-1){return dp[i][j];}
-
+        
         int up=grid[i][j]+backtrack(i-1,j,grid,dp);
         int left=grid[i][j]+backtrack(i,j-1,grid,dp);
         dp[i][j]=Math.min(up,left);
