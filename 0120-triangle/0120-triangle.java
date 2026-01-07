4a43366ef5e61ed8cade59450f9df0be4c7fbcea
Deepak <231001029@rajalakshmi.edu.in>
2026-01-07 23:48:43 +0530

    Time: 8 ms (21.33%), Space: 46.3 MB (27.39%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,34 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> tri) {
-        int ind=tri.size()-1;
-        int dp[][]=new int[ind+1][ind+1];
+        int n=tri.size()-1;
 
-        for(int i=0;i<=ind;i++){
-            dp[ind][i]=tri.get(ind).get(i);
+        ArrayList<Integer> dp=new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            dp.add(tri.get(n).get(i));
         }
 
-        for(int i=ind-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
+            ArrayList<Integer> temp=new ArrayList<>();
             for(int j=i;j>=0;j--){
-                int down=tri.get(i).get(j)+dp[i+1][j];
-                int dg=tri.get(i).get(j)+dp[i+1][j+1];
-
-                dp[i][j]=Math.min(down,dg);
+                int down=tri.get(i).get(j)+dp.get(j);
+                int dg=tri.get(i).get(j)+dp.get(j+1);
+                temp.add(0,Math.min(down,dg));
             }
+            dp=temp;
         }
-        return dp[0][0];
-
-        
-        
-      
-    }
-
-    public int backtrack(int i,int j,List<List<Integer>> tri,int ind,int [][]dp){
-        if(i==ind ){
-            return tri.get(i).get(j);}
-        if(dp[i][j]!=Integer.MAX_VALUE){return dp[i][j];}
-        int down=tri.get(i).get(j)+backtrack(i+1,j,tri,ind,dp);
-        int side=tri.get(i).get(j)+backtrack(i+1,j+1,tri,ind,dp);
-        dp[i][j]=Math.min(down,side);
-        return dp[i][j];
+        return dp.get(0);
     }
 }
