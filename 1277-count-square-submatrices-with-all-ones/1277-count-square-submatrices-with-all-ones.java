2a1eb9639c73caab9867bf9896da3478b1c541fc
Deepak <231001029@rajalakshmi.edu.in>
2026-01-25 22:13:24 +0530

    Time: 10 ms (13.64%), Space: 61.6 MB (68.9%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,35 +1,32 @@
 class Solution {
     public int countSquares(int[][] matrix) {
         int count=0;
+        int dp[][]=new int[matrix.length][matrix[0].length];
+
+        for(int i=0;i<matrix.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]!=0){
-                    count+=backtrack(i,j,matrix);
+                    count+=backtrack(i,j,matrix,dp);
                 }
             }
         }
         return count;
     }
 
-    public int backtrack(int i,int j,int[][] matrix){
-        int count=0;
-        int size=Math.min(matrix.length - i, matrix[0].length - j);
-        for(int i1=1;i1<=size;i1++){
-            if(valid(i,j,i1,matrix)){
-                count+=1;
-            }
-            else{break;}
-        }
-        return count;
-    }
+    public int backtrack(int i,int j,int[][] matrix,int [][]dp){
+        if(i<0 || j<0 || matrix[i][j]==0){return 0;}
 
-    public boolean valid(int i,int j,int c,int [][] matrix){
+        if(dp[i][j]!=-1){return dp[i][j];}
 
-        for(int a=0;a<c;a++){
-            for(int b=0;b<c;b++){
-                if(matrix[i+a][j+b]!=1){return false;}
-            }
-        }
-        return true;
-    }
+        int left=backtrack(i,j-1,matrix,dp);
+        int up=backtrack(i-1,j,matrix,dp);
+        int dig=backtrack(i-1,j-1,matrix,dp);
+
+        dp[i][j]=1+Math.min(left,Math.min(up,dig));
+        return dp[i][j];
+    }       
 }
