8f8e6d4134fb034efb11b662d6ddc7d2befa2e65
Deepak <231001029@rajalakshmi.edu.in>
2026-02-15 14:07:12 +0530

    Time: 67 ms (0%), Space: 159.2 MB (0%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.java
+++ 3840-house-robber-v/3840-house-robber-v.java
@@ -1,21 +1,22 @@
 class Solution {
     public long rob(int[] nums, int[] colors) {
-        long p1=0;
-        long p2=nums[0];
-        long p3=0;
-
-        for(int i=1;i<nums.length;i++){
-            if(colors[i]==colors[i-1]){
-                p3=nums[i]+p1;
-                p1=p2;
-                p2=Math.max(p2,p3);
-            }
-            else{
-                p3=nums[i]+p2;
-                p1=p2;
-                p2=p3;
-            }
+        long dp[][]=new long[nums.length][2];
+        for(long []i:dp){
+            Arrays.fill(i,-1);
         }
-        return Math.max(p2,p3);
+        return backtrack(0,0,nums,colors,dp);
+    }
+
+    public long backtrack(int in ,int prev,int[] nums,int colors[],long[][] dp){
+        if(in==nums.length){return 0;}
+        if(dp[in][prev]!=-1){return dp[in][prev];}
+
+        long take=0;
+        if(prev==0 || prev==1 && colors[in]!=colors[in-1]){
+            take=nums[in]+backtrack(in+1,1,nums,colors,dp);
+        }
+        long nottake=backtrack(in+1,0,nums,colors,dp);
+
+        return dp[in][prev]=Math.max(take,nottake);
     }
 }
