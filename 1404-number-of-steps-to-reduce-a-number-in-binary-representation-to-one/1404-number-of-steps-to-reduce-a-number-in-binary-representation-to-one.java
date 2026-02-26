8a0c2c492983144805c46dcaeda222badee8a4ea
Deepak <231001029@rajalakshmi.edu.in>
2026-02-26 12:07:55 +0530

    Time: 9 ms (6.49%), Space: 46.5 MB (7.37%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,21 @@
+import java.math.*;
+class Solution {
+    public int numSteps(String s) {
+        BigInteger num=new BigInteger(s,2);
+        int []c=new int[1];
+        helper(num,c);
+        return c[0];
+    }
+
+    public void helper(BigInteger num,int []c){
+        if(num.equals(BigInteger.valueOf(1))){return ;}
+        c[0]+=1;
+        if(num.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(1))){
+            helper(num.add(BigInteger.valueOf(1)),c);
+        }
+        else if(num.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))){
+            helper(num.divide(BigInteger.valueOf(2)),c);
+        }
+    }
+
+}
